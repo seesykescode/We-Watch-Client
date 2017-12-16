c9afc38ca30f6d4c0066583408448f13042ef51a
Johnathon Sykes <hireme@mrjsykes.com>
2017-12-16 12:51:11 -0800

    modified baseURL for axios call

--- src/helper.js
+++ src/helper.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
        export const request = axios.create({
-            baseURL: 'http://localhost:3001',
+            baseURL: 'http://We-Watch-Twitch-Server.herokuapp.com',
             withCredentials: true
         })
 
